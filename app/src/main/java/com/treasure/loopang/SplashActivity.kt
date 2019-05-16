01bec30cb99d6804e8a5d5f117715b77c7149701
injae <8687lee@gmail.com>
2019-05-16 14:28:07 +0900

    Splash Activity Add

---
+++ app/src/main/java/com/treasure/loopang/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.treasure.loopang
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
