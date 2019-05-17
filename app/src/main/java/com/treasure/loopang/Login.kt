2e6617b02d3a32df7d6fae000d5dfa537d59baea
injae <8687lee@gmail.com>
2019-05-17 15:54:38 +0900

    Splash Activity Bug Fix Generate Recording Activity SongManager Activity Login Activity

---
+++ app/src/main/java/com/treasure/loopang/Login.kt
@@ -0,0 +1,28 @@
+package com.treasure.loopang
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jakewharton.rxbinding3.view.clicks
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    protected val disposables by lazy { CompositeDisposable() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        login_button.clicks()
+            .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
+        gest_login_button.clicks()
+            .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
+
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/treasure/loopang/SplashActivity.kt
+++ app/src/main/java/com/treasure/loopang/SplashActivity.kt
@@ -3,13 +3,17 @@ package com.treasure.loopang
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        var wait_handler = Handler()
+        wait_handler.postDelayed({
+            setContentView(R.layout.activity_splash)
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        },1000)
     }
 }
