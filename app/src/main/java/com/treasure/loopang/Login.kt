53267a926ce0ffaa56ff6e5c67bc31d8a91bdd7f
injae <8687lee@gmail.com>
2019-05-20 12:15:07 +0900

    base update

--- app/src/main/java/com/treasure/loopang/Login.kt
+++ app/src/main/java/com/treasure/loopang/Login.kt
@@ -1,6 +1,7 @@
 package com.treasure.loopang
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.jakewharton.rxbinding3.view.clicks
@@ -10,19 +11,34 @@ import kotlinx.android.synthetic.main.activity_login.*
 class Login : AppCompatActivity() {
 
     protected val disposables by lazy { CompositeDisposable() }
+    private var permission_list = arrayOf( // 여기에 권한 추가
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.RECORD_AUDIO
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        checkPermission()
+
         login_button.clicks()
             .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
         gest_login_button.clicks()
             .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
-
     }
 
     override fun onDestroy() {
         disposables.clear()
         super.onDestroy()
+    }
+
+    fun checkPermission() {
+        for(permission in permission_list) {
+            val check = checkCallingOrSelfPermission(permission)
+            if(check == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permission_list, 0)
+                break
+            }
+        }
     }
 }
