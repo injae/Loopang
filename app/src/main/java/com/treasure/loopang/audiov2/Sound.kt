e0cf5f144ef3d1e95b285ff7c3e343b0051d4dbe
injae <8687lee@gmail.com>
2019-07-26 00:59:33 +0900

    Change:     now SoundFlow has TimeEffector bugfix save

--- app/src/main/java/com/treasure/loopang/audiov2/Sound.kt
+++ app/src/main/java/com/treasure/loopang/audiov2/Sound.kt
@@ -54,9 +54,12 @@ class Sound ( var data: MutableList<Short> = mutableListOf(),
     fun save(path: String) {
         val format = formatFactory(path)
         val fstream = FileOutputStream(path)
-        var preprocess = data.chunked(info.inputBufferSize)
-            .map{ effect(it.toShortArray()) }
-            .flatMap { it.toList() }.toMutableList()
+        //var preprocess = data.chunked(info.inputBufferSize)
+        //    .map{ effect(it.toShortArray()) }
+        //    .flatMap { it.toList() }.toMutableList()
+        var preprocess = data.chunked(info.sampleRate)
+            .map { timeEffect(it.toShortArray()).toList() }
+            .flatMap { it.chunked(info.outputBufferSize).flatMap { effect(it.toShortArray()).toList() } }.toMutableList()
         format.encord(preprocess).chunked(info.inputBufferSize).map{ it.toByteArray() }.forEach { fstream.write(it,0,it.size) }
         fstream.close()
     }
