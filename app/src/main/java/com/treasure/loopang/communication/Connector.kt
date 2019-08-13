9598ec933860615c440d5d18b9634c63926b8a06
Youn._ <dbsgmltmd23@naver.com>
2019-08-13 18:23:01 +0900

    Delete noise Package Add jjwt(Java Json Web Token) library

--- app/src/main/java/com/treasure/loopang/communication/Connector.kt
+++ app/src/main/java/com/treasure/loopang/communication/Connector.kt
@@ -1,5 +1,8 @@
 package com.treasure.loopang.communication
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.InetAddress
@@ -7,17 +10,27 @@ import java.net.Socket
 
 class Connector(val DNS: String, val port: Int, var socket: Socket = Socket(),
                 var inStream: InputStream? = null, var outStream: OutputStream? = null) {
+    private val key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+
     fun makeSocket() {
         socket = Socket(InetAddress.getByName(DNS), port)
         inStream = socket.getInputStream()
         outStream = socket.getOutputStream()
     }
 
-    fun getToken() {
+    fun makeJwtHeader() {
 
     }
 
-    fun checkToken() {
+    fun makeJwtPayload() {
 
+    }
+
+    fun makeJwtSignature() {
+
+    }
+
+    fun verifyJws() {
+        
     }
 }
