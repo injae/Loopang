f2d9badbe1ac989f7237bbd734b19401b403fa47
Sangwoon Park <nbiosupr@gmail.com>
2019-08-16 14:20:12 +0900

    Login test UI

--- app/src/main/java/com/treasure/loopang/Login.kt
+++ app/src/main/java/com/treasure/loopang/Login.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.WindowManager
+import com.afollestad.materialdialogs.MaterialDialog
 import com.jakewharton.rxbinding3.view.clicks
+import com.treasure.loopang.ui.toast
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -25,7 +28,9 @@ class Login : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
         login_button.clicks()
-            .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
+            .subscribe { onLoginButtonClick() }.apply { disposables.add(this) }
+        btn_sign_up.clicks()
+            .subscribe { onSignUpButtonClick() }.apply { disposables.add(this) }
         guest_login_button.clicks()
             .subscribe { startActivity(Intent(this, Recording::class.java)) }.apply { disposables.add(this) }
     }
@@ -43,5 +48,27 @@ class Login : AppCompatActivity() {
                 break
             }
         }
+    }
+
+    private fun onSignUpButtonClick() {
+        // not yet....
+    }
+
+    private fun onLoginButtonClick(){
+        login_button.text = getString(R.string.btn_login_wiat)
+        login_button.isClickable = false
+        val result = login(input_id.text, input_password.text)
+        login_button.text = getString(R.string.btn_sign_in)
+        if(result){
+            startActivity(Intent(this, Recording::class.java))
+        } else {
+            login_button.isClickable = true
+            toast("Login Failed")
+        }
+    }
+
+    private fun login(txtId: Editable?, txtPassword: Editable?): Boolean {
+        // 여기에 로그인관련 비지니스 로직 입력. 성공시 true 반환. 실패시 false 반환. 사실 고쳐도됨.
+        return !(txtId == null || txtPassword == null || txtId.toString() == "" || txtPassword.toString() == "")
     }
 }
