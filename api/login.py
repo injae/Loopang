fe7bcfa97b0ae0beba46c70c0e78ee8989b44a87
injae <8687lee@gmail.com>
2019-08-24 01:43:55 +0900

    bug test

--- api/login.py
+++ api/login.py
@@ -17,13 +17,14 @@ class Login(Resource):
             db_user = User.query.filter_by(email=email).first()
             if db_user is None:
                 return {'status': 'fail', 'message': 'unregistered id or wrong password'}, 200
-
+            access = Auth.encord_access_token(db_user.public_id).decode('UTF-8')
+            print(access)
             if db_user.check_password(password):
                 return {
                     'status': 'success',
                     'message': 'login',
-                    'refreshToken': Auth.encord_refresh_token(db_user.public_id).decode(),
-                    'accessToken': Auth.encord_access_token(db_user.public_id).decode()
+                    'refreshToken': Auth.encord_refresh_token(db_user.public_id).decode('UTF-8'),
+                    'accessToken': access
                 }, 200
             else:
                 return {'status': 'fail', 'message': 'unregisted id or wrong password'}, 202
