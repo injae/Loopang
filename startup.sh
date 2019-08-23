2f23862f0307fa1cebccc3ae196d9e019b4fe4d1
injae <8687lee@gmail.com>
2019-08-23 21:48:55 +0900

    update

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python manage.py db init
-python manage.py db migrate
-python manage.py db upgrade
+#python manage.py db init
+#python manage.py db migrate
+#python manage.py db upgrade
 python App.py
 echo "app run"
