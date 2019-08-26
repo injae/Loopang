8e056b05710f26ddc1dd7ac8cda62011096139ac
injae <8687lee@gmail.com>
2019-08-26 17:25:14 +0900

    bugfix

--- api/upload.py
+++ api/upload.py
@@ -7,21 +7,20 @@ import werkzeug
 class Upload(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        #parser.add_argument('token', type=str)
-        #parser.add_argument('name', type=str)
+        parser.add_argument('token', type=str)
+        parser.add_argument('name', type=str)
         parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parser.parse_args()
-        #public_id = Auth.decord_token(args['token'])
-        #print(public_id[0].get('sub'))
-        #print(args['name'])
-        #if public_id[0] is None:
-        #    return public_id[1], 200
+        public_id = Auth.decord_token(args['token'])
+        print(public_id[0].get('sub'))
+        print(args['name'])
+        if public_id[0] is None:
+            return public_id[1], 200
         if args['file'] == "":
             return {'status': 'fail', 'message': 'No File Found'}, 202
         file = args['file']
         if file:
-            #music = Music(name=args['name'], owner=public_id[0].get('sub'))
-            music = Music(name='test', owner='test')
+            music = Music(name=args['name'], owner=public_id[0].get('sub'))
             if music.save_music(file):
                 return {'status:': 'success', 'message': 'Uploaded' + music.name}, 200
             else:
