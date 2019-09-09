4465c4379cfa75157984bb1c9b788abbd71002d0
injae <8687lee@gmail.com>
2019-09-09 22:01:30 +0900

    file upload api try catch

--- api/upload.py
+++ api/upload.py
@@ -7,20 +7,23 @@ import werkzeug
 
 class Upload(Resource):
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('token', type=str)
-        parser.add_argument('name', type=str)
-        parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
-        args = parser.parse_args()
-        (token, err) = Auth.decord_token(args['token'])
-        file = args['file']
-        if token is None:
-            return err, 200
-        if file == "":
-            return request_message('fail', 'No File Found')
-        else:
-            music = Music(name=args['name'], owner=token.get('sub'))
-            if music.save_music(file):
-                return request_message('success', 'Uploaded ' + music.name)
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('token', type=str)
+            parser.add_argument('name', type=str)
+            parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+            args = parser.parse_args()
+            (token, err) = Auth.decord_token(args['token'])
+            file = args['file']
+            if token is None:
+                return err, 200
+            if file == "":
+                return request_message('fail', 'No File Found')
             else:
-                return request_message('fail', 'Is Existed file: ' + music.name)
+                music = Music(name=args['name'], owner=token.get('sub'))
+                if music.save_music(file):
+                    return request_message('success', 'Uploaded ' + music.name)
+                else:
+                    return request_message('fail', 'Is Existed file: ' + music.name)
+        except Exception as e:
+            return request_message('error', str(e))
