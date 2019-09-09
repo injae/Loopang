3da33a429a483db4fe06f0ddabf14df5f9dd8397
Youn._ <dbsgmltmd23@naver.com>
2019-09-10 00:54:28 +0900

    fix bugs & add download part

--- app/src/main/java/com/treasure/loopang/Login.kt
+++ app/src/main/java/com/treasure/loopang/Login.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.jakewharton.rxbinding3.view.clicks
+import com.treasure.loopang.Database.DatabaseManager
 import com.treasure.loopang.communication.ASyncer
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class Login : AppCompatActivity() {
     protected val disposables by lazy { CompositeDisposable() }
@@ -20,6 +23,9 @@ class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        GlobalScope.launch { DatabaseManager.deleteToken(this@Login) }
+
         setContentView(R.layout.activity_login)
         checkPermission()
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
--- app/src/main/java/com/treasure/loopang/communication/Connector.kt
+++ app/src/main/java/com/treasure/loopang/communication/Connector.kt
@@ -1,11 +1,8 @@
 package com.treasure.loopang.communication
 
 import android.os.Environment
+import android.util.Log
 import com.treasure.loopang.communication.API.LoopangNetwork
-import okhttp3.MediaType
-import okhttp3.MultipartBody
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
@@ -15,7 +12,9 @@ import java.security.SecureRandom
 import java.security.cert.X509Certificate
 import javax.net.ssl.*
 import com.treasure.loopang.communication.ResultManager.accessToken
+import okhttp3.*
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.FileOutputStream
 import java.lang.Exception
 
 class Connector(private val DNS: String = "https://ec2-3-15-172-177.us-east-2.compute.amazonaws.com",
@@ -26,19 +25,37 @@ class Connector(private val DNS: String = "https://ec2-3-15-172-177.us-east-2.co
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(getUnsafeOkHttpClient())
                     .build(),
-                private var service: LoopangNetwork = retrofit.create(LoopangNetwork::class.java)) {
+                private var service: LoopangNetwork = retrofit.create(LoopangNetwork::class.java),
+                private var file: ByteArray? = null) {
     fun process(case: Int, user: User? = null, fileName: String = ""): Result{
         var call: Call<Result>? = null
+        var fileCall: Call<ResponseBody>? = null
         var result: Result
         when(case) {
             ResultManager.AUTH -> { call = service.receiveTokens() }
             ResultManager.SIGN_UP -> { call = service.sendSignUpInfo(user!!.email, user.password, user.name) }
             ResultManager.LOGIN -> { call = service.sendLoginInfo(user!!.email, user.password) }
-            ResultManager.FILE_UPLOAD -> { call = service.sendFile(accessToken, fileName, getMultiPartBody(fileName)) } }
-        try { result = call?.execute()?.body()!! }
-        catch (e: Exception) { result = getErrorResult() }
+            ResultManager.FILE_UPLOAD -> { call = service.sendFile(accessToken, fileName, getMultiPartBody(fileName)) }
+            ResultManager.FILE_DOWNLOAD -> { fileCall = service.receiveFile(accessToken, fileName) } }
+        try {
+            if(call != null)    // 파일 다운로드가 아닌경우
+                result = call.execute().body()!!
+            else {
+                file = fileCall?.execute()?.body()?.bytes()
+                val tempFile = File("${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)}/Loopang/tt.aac")
+                val fos = FileOutputStream(tempFile)
+                fos.write(file)
+                result = getSuccessFileReceive()
+            }
+        }
+        catch (e: Exception) {
+            Log.d("OkHttp", "Error Cause : ${e}")
+            result = getErrorResult()
+        }
         return result
     }
+
+    fun getFile() = file
 }
 
 private fun getUnsafeOkHttpClient(): OkHttpClient {
@@ -67,5 +84,12 @@ private fun getMultiPartBody(fileName: String) = MultipartBody.Part
 private fun getErrorResult(): Result {
     val temp = Result()
     temp.status = "error"
+    return temp
+}
+
+private fun getSuccessFileReceive(): Result {
+    val temp = Result()
+    temp.status = "success"
+    temp.message = "file download success"
     return temp
 }
