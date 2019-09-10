186087dfe18162a62988c19afdb646841b8de591
Sangwoon Park <nbiosupr@gmail.com>
2019-09-10 23:23:08 +0900

    Record Fragment SeekBar Update

---
+++ app/src/main/java/com/treasure/loopang/FinalRecordActivity.kt
@@ -0,0 +1,12 @@
+package com.treasure.loopang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalRecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_record)
+    }
+}
