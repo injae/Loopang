24fbe53cfe67da58817cc9ceba4279e37f6ea794
sdoaolo <09jieun01@naver.com>
2019-10-27 16:44:49 +0900

    Upadate Search

--- app/src/main/java/com/treasure/loopang/CommunitySearchFragment.kt
+++ app/src/main/java/com/treasure/loopang/CommunitySearchFragment.kt
@@ -17,74 +17,84 @@ import kotlinx.android.synthetic.main.community_search.communitySearchBtn
 import kotlinx.android.synthetic.main.community_search.communitySearchEditText
 import kotlinx.android.synthetic.main.community_search.xbutton
 import kotlinx.android.synthetic.main.community_search_result.*
+import kotlin.math.log
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.R
+import android.view.KeyEvent
+import android.widget.EditText
+
+
 
 class CommunitySearchFragment : androidx.fragment.app.Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(com.treasure.loopang.R.layout.community_search,container,false);
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        var isNowSearching : Boolean = false
-        var btnSortState : String? = null
-
-        val CommunitySearchResultSongView : ListView = community_search_result_song_listview
-        val CommunitySearchResultArtistView: ListView = community_search_result_artist_listview
+        var btnSortState : String ="Tag" //tag로 초기화
+        var editResult : String = ""
+        val CommunitySearchResultTagView : ListView = community_search_result_tag_listview
+        val CommunitySearchResultUserView: ListView = community_search_result_user_listview
         val CommunitySearchAdapter: CommunitySearchitemAdapter = CommunitySearchitemAdapter()
 
-        CommunitySearchResultSongView.adapter = CommunitySearchAdapter
-        CommunitySearchResultArtistView.adapter = CommunitySearchAdapter
-
+        CommunitySearchResultTagView.adapter = CommunitySearchAdapter
+        CommunitySearchResultUserView.adapter = CommunitySearchAdapter
+/*
         CommunitySearchAdapter.addItem("punch","Done For Me","p_d")
         CommunitySearchAdapter.addItem("punch","Another Day","p_a")
         CommunitySearchAdapter.addItem("Melanie Martinez","Lunchbox Friends","m_l")
         CommunitySearchAdapter.addItem("Melanie Martinez","Orange Juice","m_o")
         CommunitySearchAdapter.addItem("Melanie Martinez","Wheels on the Bus","m_w")
         CommunitySearchAdapter.addItem("Melanie Martinez","Class Fight","m_c")
+*/
+
+        communitySearchEditText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (event.action != KeyEvent.KEYCODE_ENTER) {
+                editResult = communitySearchEditText.getText().toString()
+                Log.d("ddddddddddddddd","editReult : " + editResult)
+                return@OnKeyListener true
+            }
+            false
+        })
 
         communitySearchEditText.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {  }//텍스트 바뀌기 전
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {  }//텍스트 바뀌는 중
             override fun afterTextChanged(edit: Editable) {//텍스트 바뀐 후
-                val filterText : String = edit.toString()
-                if(btnSortState == "Song")
-                    (CommunitySearchResultSongView.getAdapter() as CommunitySearchitemAdapter).getFilter().filter(filterText)
-                else if(btnSortState == "Artist")
-                    (CommunitySearchResultArtistView.getAdapter() as CommunitySearchitemAdapter).getFilter().filter(filterText)
+               editResult = communitySearchEditText.getText().toString()
             }
         })
 
-        communitySearchBtn.setOnClickListener { isNowSearching = false }//버튼 클릭으로 검색 해야지 결과가 나옴
+        communitySearchBtn.setOnClickListener {Log.d("aaaaaaaaaaaaaaaabb","editReult : " + editResult) }//버튼 클릭으로 검색 해야지 결과가 나옴
         xbutton.setOnClickListener { communitySearchEditText.setText("") }
 
-        //초기화
-        CommunitySearchResultSongView.visibility = View.VISIBLE
-        CommunitySearchResultArtistView.visibility= View.GONE
-
-        SearchSongBtn.setOnClickListener {
-            SearchSongBtn.setTextColor(Color.argb(200, 115, 115, 115))
-            SearchSongBtn.setBackgroundColor(Color.WHITE)
-            SearchArtistBtn.setBackgroundColor(Color.argb(0, 0, 0, 0))
-            SearchArtistBtn.setTextColor(Color.WHITE)
-            btnSortState = "Song"
-            CommunitySearchResultSongView.visibility = View.VISIBLE
-            CommunitySearchResultArtistView.visibility= View.GONE
+        SearchTagBtn.setOnClickListener {
+            SearchTagBtn.setTextColor(Color.argb(200, 115, 115, 115))
+            SearchTagBtn.setBackgroundColor(Color.WHITE)
+            SearchUserBtn.setBackgroundColor(Color.argb(0, 0, 0, 0))
+            SearchUserBtn.setTextColor(Color.WHITE)
+            btnSortState = "Tag"
+            CommunitySearchResultTagView.visibility = View.VISIBLE
+            CommunitySearchResultUserView.visibility= View.GONE
         }
-        SearchArtistBtn.setOnClickListener {
-            SearchArtistBtn.setTextColor(Color.argb(200, 115, 115, 115))
-            SearchArtistBtn.setBackgroundColor(Color.WHITE)
-            SearchSongBtn.setBackgroundColor(Color.argb(0, 0, 0, 0))
-            SearchSongBtn.setTextColor(Color.WHITE)
-            btnSortState = "Artist"
-            CommunitySearchResultSongView.visibility = View.GONE
-            CommunitySearchResultArtistView.visibility= View.VISIBLE
+        
+        SearchUserBtn.setOnClickListener {
+            SearchUserBtn.setTextColor(Color.argb(200, 115, 115, 115))
+            SearchUserBtn.setBackgroundColor(Color.WHITE)
+            SearchTagBtn.setTextColor(Color.WHITE)
+            SearchTagBtn.setBackgroundColor(Color.argb(0, 0, 0, 0))
+            btnSortState = "User"
+            CommunitySearchResultTagView.visibility = View.GONE
+            CommunitySearchResultUserView.visibility= View.VISIBLE
         }
 
-        CommunitySearchResultArtistView.onItemClickListener=AdapterView.OnItemClickListener{ parent, v, position, id ->
+        CommunitySearchResultUserView.onItemClickListener=AdapterView.OnItemClickListener{ parent, v, position, id ->
             val item = parent.getItemAtPosition(position) as CommunitySongItem
             activity!!.TrackFrame.visibility = View.VISIBLE
             (activity as CommunityActivity).onFragmentChangedtoTrack(item.songId)
         }
-        CommunitySearchResultSongView.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+        CommunitySearchResultTagView.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
             val item = parent.getItemAtPosition(position) as CommunitySongItem
             activity!!.TrackFrame.visibility = View.VISIBLE
             (activity as CommunityActivity).onFragmentChangedtoTrack(item.songId)
