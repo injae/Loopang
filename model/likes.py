9af129c659073f2af7bb67e9b5931b62d9bb59b5
injae <8687lee@gmail.com>
2019-11-19 15:50:02 +0900

    NonType attribute bugfix

--- model/likes.py
+++ model/likes.py
@@ -21,14 +21,14 @@ class Like(db.Model):
     def off(self):
         is_find = Like.query.filter_by(user_id=self.user_id, music_id=self.music_id).first()
         if is_find is not None:
-            is_find.music.likes -= 1
+            is_find.music_likes.likes -= 1
             db.session.delete(is_find)
             db.session.commit()
 
     def on(self):
         is_find = Like.query.filter_by(user_id=self.user_id, music_id=self.music_id).first()
         if is_find is None:
-            self.music.likes += 1
+            self.music_likes.likes += 1
             db.session.add(self)
             db.session.commit()
             return True
