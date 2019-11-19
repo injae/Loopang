1f5aaa8ba87889e6a549e3932d50a1f559f4d2e6
injae <8687lee@gmail.com>
2019-11-19 16:16:31 +0900

    docker locale setting and likes bugfix

--- model/likes.py
+++ model/likes.py
@@ -1,4 +1,4 @@
-from model.database import db
+from model.database import db, insert, update, remove
 from model.music import Music
 
 
@@ -21,16 +21,15 @@ class Like(db.Model):
     def off(self):
         is_find = Like.query.filter_by(user_id=self.user_id, music_id=self.music_id).first()
         if is_find is not None:
-            is_find.music_likes.likes -= 1
-            db.session.delete(is_find)
-            db.session.commit()
+            is_find.music.likes -= 1
+            remove(is_find)
 
     def on(self):
         is_find = Like.query.filter_by(user_id=self.user_id, music_id=self.music_id).first()
         if is_find is None:
-            self.music_likes.likes += 1
-            db.session.add(self)
-            db.session.commit()
+            insert(self)
+            self.music.likes += 1
+            update()
             return True
         else:
             return False
