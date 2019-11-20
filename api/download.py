d5f441969b1e2ca318dafe7b70494aaecb1e8806
injae <8687lee@gmail.com>
2019-11-20 22:31:19 +0900

    download api new argument preplay

--- api/download.py
+++ api/download.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, inputs
 from flask import Response, stream_with_context
 from tools.request_message import request_message
 from api.auth import Auth
@@ -14,19 +14,23 @@ class Download(Resource):
             parser = reqparse.RequestParser()
             parser.add_argument('token', type=str)
             parser.add_argument('music_id', type=str)
+            parser.add_argument('preplay', type=inputs.boolean)
             args = parser.parse_args()
 
             (token, err) = Auth.decord_token(args['token'])
             if token is None:
                 return err, 200
 
+            is_preplay = args['preplay']
             _file = Music.query.filter_by(music_id=args['music_id']).first()
-            logger().debug('[download] file: %s', _file.music_id)
-            logger().debug('[download] file: %s', _file.name)
+            logger().debug('[download] music id: %s', _file.music_id)
+            logger().debug('[download] file name: %s', _file.name)
+            logger().debug('[download] is preplay: %s', is_preplay)
             if _file is not None:
                 if(os.path.exists(_file.path())):
                     buffer = open(_file.path(), 'rb')
-                    _file.downloads += 1
+                    if not is_preplay:
+                        _file.downloads += 1
                     update()
                     def streaming():
                         for line in buffer:
