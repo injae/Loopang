3307761b54941798eae8c7f475353ab271d90757
sdoaolo <09jieun01@naver.com>
2019-11-20 23:16:59 +0900

    Restore share adapter

--- app/src/main/java/com/treasure/loopang/CommunityUserPageFragment.kt
+++ app/src/main/java/com/treasure/loopang/CommunityUserPageFragment.kt
@@ -82,13 +82,11 @@ class CommunityUserPageFragment : androidx.fragment.app.Fragment() {
             val itt = parent.getItemAtPosition(position) as MusicListClass
             activity!!.TrackFrame.visibility = View.VISIBLE
             (activity as CommunityActivity).onFragmentChangedtoTrack(itt)
-            Log.d("tttttttttt","itt.tag: "+itt.tags)
         }
         userPageLikedListView.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
             val itt = parent.getItemAtPosition(position) as MusicListClass
             activity!!.TrackFrame.visibility = View.VISIBLE
             (activity as CommunityActivity).onFragmentChangedtoTrack(itt)
-            Log.d("tttttttttt","itt.tag: "+itt.tags)
         }
         addTrackButton.setOnClickListener {
             val intent = Intent(activity, CommunityShareActivity::class.java)
